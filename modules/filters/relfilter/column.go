e7933b6e78b44eb97822b9c31d7a7491b21126e9
Boris Ershov <b.ershov@nixys.ru>
2024-08-08 17:27:09 +0700

    feat(#35): Add capturing groups for column filters

--- modules/filters/relfilter/column.go
+++ modules/filters/relfilter/column.go
@@ -1,13 +1,22 @@
 package relfilter
 
+import "fmt"
+
 type columns struct {
 	cc []*column
 	m  map[string]*column
 }
 
 type column struct {
-	n       string
-	rawType string
+	n string
+	t columnTypes
+}
+
+type columnTypes struct {
+	raw    string
+	groups [][]string
+	r      *ColumnRuleOpts
+	env    []string
 }
 
 func columnsInit() columns {
@@ -17,11 +26,37 @@ func columnsInit() columns {
 	}
 }
 
-func (c *columns) add(name string, rt string) {
+func (c *columns) add(name string, rt string, pts [][]string, r *ColumnRuleOpts) {
+
+	env := []string{fmt.Sprintf("%s=%s", envVarColumnTypeRAW, rt)}
+
+	if pts != nil {
+		for i, g := range pts {
+			for j, sg := range g {
+
+				if j == 0 {
+					env = append(
+						env,
+						fmt.Sprintf("%s%d=%s", envVarColumnTypeGroupPrefix, i, sg),
+					)
+				} else {
+					env = append(
+						env,
+						fmt.Sprintf("%s%d_%d=%s", envVarColumnTypeGroupPrefix, i, j-1, sg),
+					)
+				}
+			}
+		}
+	}
 
 	v := column{
-		n:       name,
-		rawType: rt,
+		n: name,
+		t: columnTypes{
+			raw:    rt,
+			groups: pts,
+			r:      r,
+			env:    env,
+		},
 	}
 
 	c.cc = append(c.cc, &v)
